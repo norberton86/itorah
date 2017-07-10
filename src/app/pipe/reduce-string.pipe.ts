654e7273d310743a2a35b2806323d27dbc0f2937
norberton86 <norberton86@gmail.com>
2017-07-10 14:08:09 -0400

    Custom Pipe

--- src/app/pipe/reduce-string.pipe.ts
+++ src/app/pipe/reduce-string.pipe.ts
@@ -6,8 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ReduceStringPipe implements PipeTransform {
 
   transform(value: string): string {
-    if(value.length>38)
-       value=value.substring(0,35)+"...";
+    if(value.length>29)
+       value=value.substring(0,29)+"...";
 
       return value;
   }

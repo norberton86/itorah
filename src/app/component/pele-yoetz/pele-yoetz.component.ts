87025025adac8cd6582c5b0bd0fc4d27c97c8c6e
norberton86 <norberton86@gmail.com>
2017-07-25 14:14:13 -0400

    Download

--- src/app/component/pele-yoetz/pele-yoetz.component.ts
+++ src/app/component/pele-yoetz/pele-yoetz.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, NgZone,Input } from '@angular/core';
+import { Component, OnInit, NgZone, Input } from '@angular/core';
 
 
 import { Pele } from '../../model/Pele';
@@ -11,30 +11,31 @@ declare var $: any;
   selector: 'app-pele-yoetz',
   templateUrl: './pele-yoetz.component.html',
   styleUrls: ['./pele-yoetz.component.css'],
-  providers: [PeleService,PlayerService]
+  providers: [PeleService, PlayerService]
 })
 export class PeleYoetzComponent implements OnInit {
 
   peles: Array<Pele> = [];
   query_main: string = '';
-  amount:number=0;
+  amount: number = 0;
+  
+  currentId:string=""
 
- @Input()
-   accion:string="";
-   rendering:boolean=false;
+  @Input()
+  accion: string = "";
+  rendering: boolean = false;
 
 
-  constructor(private peleService: PeleService, private ngZone: NgZone,private playerService:PlayerService) {
+  constructor(private peleService: PeleService, private ngZone: NgZone, private playerService: PlayerService) {
 
   }
 
-   ngOnChanges(changes:any) {
-     if(changes.accion!=null&&!changes.accion.firstChange)
-     {
-       this.rendering=true;   
-       this.RefreshView();
-     }
-      
+  ngOnChanges(changes: any) {
+    if (changes.accion != null && !changes.accion.firstChange) {
+      this.rendering = true;
+      this.RefreshView();
+    }
+
   }
 
   ngOnInit() {
@@ -61,7 +62,7 @@ export class PeleYoetzComponent implements OnInit {
       this.peles = this.peles.filter(function (s) {
         return s.title.toLowerCase().includes(query.toLowerCase());
       });
-      
+
     }
     this.RefreshView()
 
@@ -70,9 +71,9 @@ export class PeleYoetzComponent implements OnInit {
 
   RefreshView() {
 
-    this.amount=this.peles.length;
-    if(!this.rendering)  //the first time don't renderize
-        return;
+    this.amount = this.peles.length;
+    if (!this.rendering)  //the first time don't renderize
+      return;
 
     let self = this;
 
@@ -81,23 +82,54 @@ export class PeleYoetzComponent implements OnInit {
       $('#ballon').html($('#item-content-5').html());
       $('#ballon .search-btn').click(function () {
 
-       self.Search();
+        self.Search();
 
       })
 
-      $('#ballon form').submit(function(e){
-         e.preventDefault();
-         self.query_main= $('#ballon [type="search"]').val()
-         self.Search();
+      $('#ballon form').submit(function (e) {
+        e.preventDefault();
+        self.query_main = $('#ballon [type="search"]').val()
+        self.Search();
 
       })
 
-      $('#ballon .play-secondary').click(function(){
+      $('#ballon .play-secondary').click(function () {
 
-         var title=$(this).attr('title');
-        var url=$(this).attr('data-url');
-         self.playerService.Play2(title,url)     
-         
+        var title = $(this).attr('title');
+        var url = $(this).attr('data-url');
+        self.playerService.Play2(title, url)
+
+      })
+
+      $.contextMenu({
+        selector: '#ballon .link-more',
+        trigger: 'left',
+        callback: function (key, options) {
+
+          var p =self.peles.filter(function (s) {
+                 return s.id== parseInt( self.currentId);
+               })[0];
+
+        $('#ballon #download').attr('href',p.audioUrl);  //http://itorah.3nom.com/music.mp3
+
+          
+          switch(key)
+          {
+             case 'hebrewPdf':  window.open(p.hebrewPDF); break;
+             case 'englishPdf': window.open(p.englishPDF); break;
+             case 'mp3':document.getElementById('download').click(); break;
+          }
+        },
+        items: {
+          "hebrewPdf": { name: "Hebrew PDF", icon: "fa-file-pdf-o" },
+          "englishPdf": { name: "English PDF", icon: "fa-file-pdf-o" },
+          "mp3": { name: "Mp3", icon: "fa-file-audio-o" }
+        }
+      });
+
+      $('#ballon .link-more').click(function(){  //click over more...
+
+          self.currentId=$(this).attr('data-value') 
       })
 
       $('#ballon .search-field').val(self.query_main);
@@ -106,21 +138,20 @@ export class PeleYoetzComponent implements OnInit {
   }
 
 
-  Search()
-  {
+  Search() {
 
-  let self=this;
+    let self = this;
 
- self.ngZone.run(() => {
+    self.ngZone.run(() => {
 
-          self.query_main = $('#ballon .search-field').val();  //update the query field in my component (remenber double data binding)
+      self.query_main = $('#ballon .search-field').val();  //update the query field in my component (remenber double data binding)
 
-          if (localStorage.getItem("peles") != null || localStorage.getItem("peles") != '') {
-            self.peles = JSON.parse(localStorage.getItem("peles"));  //recover the originals
-          }
+      if (localStorage.getItem("peles") != null || localStorage.getItem("peles") != '') {
+        self.peles = JSON.parse(localStorage.getItem("peles"));  //recover the originals
+      }
 
-          self.Update();
-        })
+      self.Update();
+    })
 
 
   }

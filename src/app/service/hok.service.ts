46261f62c305caae2a258f6feaddea6720271418
norberton86 <norberton86@gmail.com>
2017-08-02 12:30:15 -0400

    More Data

--- src/app/service/hok.service.ts
+++ src/app/service/hok.service.ts
@@ -67,8 +67,58 @@ export class HokService extends Service{
     "language": "English",
     "audio": "http://peleyoetz.com/PeleYoetz/3.mp3",
     "video": "",
-    "dayWeek":"Tuesday",
+    "dayWeek":"Monday",
     "myClass":"Additional"
+  },
+  {
+    "title": "Tomer Devorah Summer 2017 Part 1",
+    "dateRecorded": "2017-07-19T00:00:00",
+    "length": "60:0         ",
+    "language": "English",
+    "audio": "http://peleyoetz.com/PeleYoetz/3.mp3",
+    "video": "",
+    "dayWeek":"Tuesday",
+    "myClass":"Class"
+  },
+  {
+    "title": "Tomer Devorah Summer 2017 Part 2",
+    "dateRecorded": "2017-07-19T00:00:00",
+    "length": "60:0         ",
+    "language": "English",
+    "audio": "http://peleyoetz.com/PeleYoetz/3.mp3",
+    "video": "",
+    "dayWeek":"Wednesday",
+    "myClass":"Additional"
+  },
+  {
+    "title": "The Missing Letter / 21 Days",
+    "dateRecorded": "2017-07-17T00:00:00",
+    "length": "60:0         ",
+    "language": "English",
+    "audio": "http://peleyoetz.com/PeleYoetz/3.mp3",
+    "video": "",
+    "dayWeek":"Thursday",
+    "myClass":"Class"
+  },
+  {
+    "title": "Vayaamod Pinhas / Challenges",
+    "dateRecorded": "2017-07-17T00:00:00",
+    "length": "60:0         ",
+    "language": "English",
+    "audio": "http://peleyoetz.com/PeleYoetz/3.mp3",
+    "video": "",
+    "dayWeek":"Friday",
+    "myClass":"Additional"
+  },
+  {
+    "title": "Perush Rashi on Parashat Korah",
+    "dateRecorded": "2017-07-10T00:00:00",
+    "length": "60:0         ",
+    "language": "English",
+    "audio": "http://peleyoetz.com/PeleYoetz/3.mp3",
+    "video": "",
+    "dayWeek":"Saturday",
+    "myClass":"Class"
   }]);
             else
             observer.next([{
@@ -78,7 +128,7 @@ export class HokService extends Service{
     "language": "English",
     "audio": "http://peleyoetz.com/PeleYoetz/4.mp3",
     "video": "",
-    "dayWeek":"Wednesday",
+    "dayWeek":"Sunday",
     "myClass":"Rashi"
   },
   {
@@ -88,8 +138,58 @@ export class HokService extends Service{
     "language": "English",
     "audio": "http://peleyoetz.com/PeleYoetz/5.mp3",
     "video": "",
-    "dayWeek":"Thursday",
+    "dayWeek":"Monday",
     "myClass":"Class"
+  },
+  {
+    "title": "Perush Rashi on Parashat Hukat",
+    "dateRecorded": "2017-07-10T00:00:00",
+    "length": "60:0         ",
+    "language": "English",
+    "audio": "http://peleyoetz.com/PeleYoetz/5.mp3",
+    "video": "",
+    "dayWeek":"Tuesday",
+    "myClass":"Rashi"
+  },
+  {
+    "title": "The Aderet / Jewish Home",
+    "dateRecorded": "2017-07-07T00:00:00",
+    "length": "60:0         ",
+    "language": "English",
+    "audio": "http://peleyoetz.com/PeleYoetz/5.mp3",
+    "video": "http://media.learntorah.com/LT-Video/mp4:LBM227.mp4/playlist.m3u8",
+    "dayWeek":"Wednesday",
+    "myClass":"Class"
+  },
+  {
+    "title": "Jewish Home Part 2",
+    "dateRecorded": "2017-07-07T00:00:00",
+    "length": "60:0         ",
+    "language": "English",
+    "audio": "http://peleyoetz.com/PeleYoetz/5.mp3",
+    "video": "",
+    "dayWeek":"Thursday",
+    "myClass":"Rashi"
+  },
+  {
+    "title": "The 10 Coverings / Emunah",
+    "dateRecorded": "2017-07-02T00:00:00",
+    "length": "0:0         ",
+    "language": "English",
+    "audio": "http://peleyoetz.com/PeleYoetz/5.mp3",
+    "video": "",
+    "dayWeek":"Friday",
+    "myClass":"Class"
+  },
+  {
+    "title": "The Well Of Miriam",
+    "dateRecorded": "2017-07-02T00:00:00",
+    "length": "60:0         ",
+    "language": "English",
+    "audio": "http://peleyoetz.com/PeleYoetz/5.mp3",
+    "video": "",
+    "dayWeek":"Saturday",
+    "myClass":"Rashi"
   }]);
            
         

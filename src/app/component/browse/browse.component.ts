2a3aa642e9900107584a754908759b80569abc81
norberton86 <norberton86@gmail.com>
2018-01-02 12:07:49 -0500

    UI Format changed

--- src/app/component/browse/browse.component.ts
+++ src/app/component/browse/browse.component.ts
@@ -5,6 +5,7 @@ import { ItemQueue, Category } from '../../model/shiurim';
 import { Observable } from 'rxjs/Observable';
 import { Speaker } from '../../model/speaker';
 import { SpeakerService } from '../../service/speaker.service';
+import { Page } from '../../model/page';
 
 @Component({
   selector: 'app-browse',
@@ -23,6 +24,8 @@ export class BrowseComponent implements OnInit, OnChanges {
   recently: Array<ItemQueue> = []
   popular: Array<ItemQueue> = []
   relevant: Array<ItemQueue> = []
+  
+  allBrowse: Array<ItemQueue> = []
   browse: Array<ItemQueue> = []
 
   loading: boolean = false
@@ -52,7 +55,7 @@ export class BrowseComponent implements OnInit, OnChanges {
   Category() {
     
     if (this.category.id != 0) {
-      this.browse = []
+      this.allBrowse = []
       let self = this
       this.loadingCategory=true
       this.browseService.readCategory(this.category.id).subscribe(function (response) {
@@ -60,10 +63,12 @@ export class BrowseComponent implements OnInit, OnChanges {
         if(self.speaker.id!=0)
         {
           var complete=self.speaker.title+" "+self.speaker.firstName+" "+self.speaker.lastName
-          self.browse = response.filter(i=>i.speaker==complete)
+          self.allBrowse = response.filter(i=>i.speaker==complete)
         }
         else
-        self.browse = response
+        self.allBrowse = response
+
+        self.Update()
 
         self.loadingCategory=false
       }, function (error) { 
@@ -120,10 +125,11 @@ export class BrowseComponent implements OnInit, OnChanges {
       );
   }
 
-  Play(id: string, title: string,speaker:string,sponsor:string,mediaId:string) {
+ Play(id: string, title: string, sponsor: string,mediaId:string) {
     var onlyAudio = title.includes('LT-Audio');
-    this.playerService.Play(title, id, onlyAudio,speaker,sponsor,1,mediaId);
+    this.playerService.Play(title, id, onlyAudio, this.speaker.firstName + " " + this.speaker.lastName, sponsor,1,mediaId);
   }
+
 
   Desc(a, b) {
     if (a.date < b.date)
@@ -156,5 +162,78 @@ export class BrowseComponent implements OnInit, OnChanges {
   setCurrent(c: string) {
     this.current = c
   }
+
+  //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  pages: Array<Page>;
+  allPages: number;
+  iteration: number;
+  allIteration: number;
+  elem: number = 28
+
+  PagingPrev() {
+    this.iteration--;
+    if (this.iteration <= 0) {
+      this.iteration = 1;
+    }
+    else
+      this.CreatePages();
+  }
+
+  PagingNext() {
+    this.iteration++;
+    if (this.iteration > Math.ceil(this.allPages / 6)) {
+      this.iteration = Math.ceil(this.allPages / 6);
+    }
+    else
+      this.CreatePages();
+  }
+
+  Page(id: number) {
+
+    this.pages.forEach(function (p) {
+
+      if (p.id != id)
+        p.current = false;
+      else
+        p.current = true;
+    })
+
+    this.PopulatedShirium(id);
+
+  }
+
+    CreatePages() {
+    this.pages = [];
+
+    for (var i = this.iteration * 6 - 6; i < this.iteration * 6 && i < this.allPages; i++) //populate the pages array
+    {
+      if (i == (this.iteration - 1) * 6) {
+        this.pages.push({ id: i + 1, current: true });
+        this.PopulatedShirium(i + 1);  //the page            
+      }
+      else
+        this.pages.push({ id: i + 1, current: false });
+    }
+
+  }
+
+  PopulatedShirium(id: number) {
+    this.browse = [];
+    for (var i = id * this.elem - this.elem; i < id * this.elem && i < this.allBrowse.length; i++) {
+      this.browse.push(this.allBrowse[i]);  //populate the grid
+    }
+
+  }
+
+  Update() {
+
+    this.allPages = this.allBrowse.length / this.elem; //pagination
+    this.iteration = 1; //pagination
+
+    this.CreatePages();
+
+  }
+
 
 }

bb37c444cb3ee051ae2ee1adb228d53e31d76862
norberton86 <norberton86@gmail.com>
2018-02-06 15:55:17 -0500

    Added

--- src/app/component/place-holder/place-holder.component.ts
+++ src/app/component/place-holder/place-holder.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UploadService } from '../../service/upload.service';
 
+declare var $:any
+
 @Component({
   selector: 'app-place-holder',
   templateUrl: './place-holder.component.html',
@@ -9,8 +11,8 @@ import { UploadService } from '../../service/upload.service';
 })
 export class PlaceHolderComponent implements OnInit {
 
-  left:string="/assets/build/css/images/temp/left.png"
-  right:string="/assets/build/css/images/temp/right.png"
+  left: string = "/assets/build/css/images/temp/left.png"
+  right: string = "/assets/build/css/images/temp/right.png"
   constructor(private uploadService: UploadService) { }
 
   ngOnInit() {
@@ -23,6 +25,8 @@ export class PlaceHolderComponent implements OnInit {
     )*/
   }
 
-
+  OpenAdvertise() {
+     $('#popup-advertise').toggleClass('shown');
+  }
 
 }

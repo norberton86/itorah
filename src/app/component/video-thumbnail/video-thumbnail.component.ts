26473ff700a0f365fe8ca89857bf3bc21145b5a6
norberton86 <norberton86@gmail.com>
2018-02-12 09:23:29 -0500

    first version slick

--- src/app/component/video-thumbnail/video-thumbnail.component.ts
+++ src/app/component/video-thumbnail/video-thumbnail.component.ts
@@ -193,4 +193,28 @@ export class VideoThumbnailComponent implements OnInit {
     return arr[1] + arr[2] + ".png"
   }
 
+  //--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  slides = [
+    {img: "http://placehold.it/350x150/000000"},
+    {img: "http://placehold.it/350x150/111111"},
+    {img: "http://placehold.it/350x150/333333"},
+    {img: "http://placehold.it/350x150/666666"},
+    {img: "http://placehold.it/350x150/000000"},
+    {img: "http://placehold.it/350x150/111111"},
+    {img: "http://placehold.it/350x150/333333"},
+    {img: "http://placehold.it/350x150/666666"}
+  ];
+  slideConfig = {"slidesToShow": 4, "slidesToScroll": 1};
+
+  addSlide() {
+    this.slides.push({img: "http://placehold.it/350x150/777777"})
+  }
+
+  removeSlide() {
+    this.slides.length = this.slides.length - 1;
+  }
+  afterChange(e) {
+    console.log('afterChange');
+  }
 }
